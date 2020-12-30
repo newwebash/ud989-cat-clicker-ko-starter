54bb03ffd4f32d8222fe80803bc7ed1af237db14
newwebash <newtona2011@gmail.com>
2020-12-30 13:50:57 -0500

    Implement levels although in a hacky way

--- js/app.js
+++ js/app.js
@@ -7,6 +7,20 @@ var ViewModel = function() {
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
     }
+
+    this.level1 = ko.computed(function() {
+        return this.clickCount() <5;
+    }, this);
+
+    this.level2 = ko.computed(function() {
+        return this.clickCount() >=5 && this.clickCount() <=10;
+    }, this);
+
+    this.level3 = ko.computed(function() {
+        return this.clickCount() >10;
+    }, this);
+
+
 }
 
 ko.applyBindings(new ViewModel())
